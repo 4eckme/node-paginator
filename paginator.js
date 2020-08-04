c8aced8eb8a2d915f4bab39080bd080d77367ea1
4eckme <60138564+4eckme@users.noreply.github.com>
2020-08-04 19:11:16 +0300

    Create paginator.js

---
+++ paginator.js
@@ -0,0 +1,42 @@
+var paginator = function() { return {
+	page: 1,
+	limit: 100,
+	count: 0,
+	pages: 0,
+	length: 0,
+	selected_page_href: 1,
+	set_limit: function (l) {
+		this.count = parseInt(l);
+	},
+	set_page: function (p) {
+		this.page = parseInt(p) ? parseInt(p) : 1
+	},
+	set_count: function (c) {
+		this.count = parseInt(c);
+	},
+	set_length: function(l) {
+		this.length = parseInt(l)
+	},
+	sql_limit: function() {
+		return ' LIMIT '+this.limit*(this.page-1)+', '+this.limit+' ';
+	},
+	html_links: function (href) {
+		chain = Array();
+		this.pages = Math.ceil(this.count/this.limit);
+		for(i=1; i<=this.pages; i++) {
+			classes = Array();
+			hrefstr = (((i != this.page) || this.selected_page_href) ? (' href="'+href.replace(':page', i)+'"') : '');			
+			if (i == 1) classes.push("p-start");
+			if (i == this.pages) classes.push("p-end");
+			if (i == this.page) classes.push("p-selected");
+			if (this.length && Math.abs(i-this.page) <= this.length/2) classes.push("p-center");
+			clss = (classes.length ? (' class="'+classes.join(' ')+'"') : '');
+			if(!this.length || classes.length) {
+				chain.push('<a'+hrefstr+clss+'>'+i+'</a>');
+			}
+		}
+		return chain.join('');
+	}	
+}}
+
+module.exports = paginator;
