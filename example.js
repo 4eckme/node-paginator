f6b43b7bd715ed52409866476ac0fdef9aa7c887
4eckme <60138564+4eckme@users.noreply.github.com>
2020-08-04 23:40:43 +0300

    Update example.js

--- example.js
+++ example.js
@@ -1,19 +1,19 @@
-paginator = require('./paginator');
+// Create modules object
+paginator = require('./paginator.js');
 pgntr = new paginator();
-
-pgntr.set_page(3); //default 1;
-pgntr.set_limit(20); //default 100;
-
-connection.query('SELECT count(id) as cnt FROM pages; select * from pages '+pgntr.sql_limit(), {}, function (e,r,f){
-
-  pgntr.set_count(r[0][0].cnt) // before html_links()
-  
-  console.log(pgntr.html_links('/page/:page'));
-  console.log(pgntr.html_links('/page/:page', 0, 0));
-  console.log(pgntr.html_links('/page/:page', 5, 1));
-  console.log(pgntr.html_links('/page/:page', 5, 0));
-  // arguments: link template (string), paginations central pages length (int), clickable of selected page (bool)
-  
-  console.log(pgntr.index()) // autoicrement of element index, start from the index of the first element of the page
-  
-})
+// Settings
+pgntr.set_page(3); // default 1;
+pgntr.set_limit(20); // default 100;
+// SQL with limits
+connection.query('select * from pages '+pgntr.sql_limit());
+// Set items count (before use html_links())
+pgntr.set_count(100);
+// Output pages list
+res.send(pgntr.html_links('/page/:page'));
+res.send(pgntr.html_links('/page/:page', 0, 0));
+res.send(pgntr.html_links('/page/:page', 5, 0));
+res.send(pgntr.html_links('/page/:page', 5, 1));
+res.send(pgntr.html_links('/page/:page'));
+/* Att last call html_links, module will use last params set */
+res.send(pgntr.index())//Auto increment of item index, started from index of first element on this page
+/* last method can be used if yo want show item index to client, it can be used in cycle, displayed items *
