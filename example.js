1d9b495ed28bb8c85bdadbfba0610de861b1e381
4eckme <60138564+4eckme@users.noreply.github.com>
2020-08-04 19:52:11 +0300

    Update example.js

--- example.js
+++ example.js
@@ -4,15 +4,14 @@ pgntr = new paginator();
 pgntr.set_page(3); //default 1;
 pgntr.set_limit(20); //default 100;
 
-connection.query('SELECT count(id) as cnt FROM pages; select * from pages'+pgntr.sql_limit(), {}, function (e,r,f){
+connection.query('SELECT count(id) as cnt FROM pages; select * from pages '+pgntr.sql_limit(), {}, function (e,r,f){
 
   pgntr.set_count(r[0][0].cnt) // before html_links()
   
   console.log(pgntr.html_links('/page/:page'));
+  console.log(pgntr.html_links('/page/:page', 0, 0));
+  console.log(pgntr.html_links('/page/:page', 5, 1));
+  console.log(pgntr.html_links('/page/:page', 5, 0));
+  // arguments: link template (string), paginations central pages length (int), clickable of selected page (bool)
   
-  pgntr.set_length(5); // default 0 - no cut (set length of selected page neighborhoods)
-  console.log(pgntr.html_links('/page/:page'));
-  
-  pgntr.selected_page_href = 0; // default 1 (sets clickable of selected page)
-  console.log(pgntr.html_links('/page/:page'));
 })
